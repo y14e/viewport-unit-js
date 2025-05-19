4e66bfdbdbb4b239b29aa424c36f4b2752042734
Yusuke Kamiyamane <y@y14e.com>
2025-05-19 22:36:39 +0900

    20250519

--- viewport-unit.ts
+++ viewport-unit.ts
@@ -2,11 +2,13 @@ export function updateViewportUnit(): void {
   const htmlElement = document.documentElement;
   const width = htmlElement.clientWidth / 100;
   const height = htmlElement.clientHeight / 100;
-  Object.assign(htmlElement.style, {
+  Object.entries({
     '--vw': String(width),
     '--vh': String(height),
     '--vmin': String(Math.min(width, height)),
     '--vmax': String(Math.max(width, height)),
+  }).forEach(([name, value]) => {
+    htmlElement.style.setProperty(name, value);
   });
 }
 

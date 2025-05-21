6df9864f4a1250f8a2fc7c8a6e723993087f7b3d
Yusuke Kamiyamane <y@y14e.com>
2025-05-21 19:57:53 +0900

    20250521a

--- viewport-unit.ts
+++ viewport-unit.ts
@@ -1,8 +1,8 @@
 export function updateViewportUnit(): void {
   const htmlElement = document.documentElement;
+  const isHorizontal = window.getComputedStyle(htmlElement).getPropertyValue('writing-mode').startsWith('horizontal');
   const width = htmlElement.clientWidth / 100;
   const height = htmlElement.clientHeight / 100;
-  const isHorizontal = window.getComputedStyle(htmlElement).getPropertyValue('writing-mode').startsWith('horizontal');
   Object.entries({
     '--vi': String(isHorizontal ? width : height),
     '--vb': String(isHorizontal ? height : width),

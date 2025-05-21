654e6054949d2302253f144a755fb33cfbe6e7c3
Yusuke Kamiyamane <y@y14e.com>
2025-05-21 20:04:32 +0900

    20250521b

--- viewport-unit.js
+++ viewport-unit.js
@@ -1,13 +1,13 @@
 export function updateViewportUnit() {
   const htmlElement = document.documentElement;
-  const isHorizontal = window.getComputedStyle(htmlElement).getPropertyValue('writing-mode').startsWith('horizontal');
   const width = htmlElement.clientWidth / 100;
   const height = htmlElement.clientHeight / 100;
+  const isHorizontal = window.getComputedStyle(htmlElement).getPropertyValue('writing-mode').startsWith('horizontal');
   Object.entries({
-    '--vi': String(isHorizontal ? width : height),
-    '--vb': String(isHorizontal ? height : width),
     '--vw': String(width),
     '--vh': String(height),
+    '--vi': String(isHorizontal ? width : height),
+    '--vb': String(isHorizontal ? height : width),
     '--vmin': String(Math.min(width, height)),
     '--vmax': String(Math.max(width, height)),
   }).forEach(([name, value]) => {
